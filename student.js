e673c4bd79889f01d455257ad622d025bc21e5f8
srivastavravikuamr-lang <srivastavravikuamr@gmail.com>
2026-04-07 16:57:10 +0530

    Update student.js

--- student.js
+++ student.js
@@ -83,10 +83,10 @@ var students = [
 
 // PROFESSORS
 var professors = [
-  {name: "Dr. MANJULA DAS mam", subject: "ENGINEERING MECHANICS"},
-  {name: "Dr. SUBASIS BENARJEE sir", subject: "DSA"},
-  {name: "Dr. SAM AHMED MAZUMDER sir", subject: "HUMANITIES"},
-  {name: "Dr. SUPREME DAS sir", subject: "METALLURGY"}
+  {name: "Dr.MANJULA DAS mam", subject: "ENGINEERING MECHANICS"},
+  {name: "Dr.SUBASIS BENARJEE sir", subject: "DSA"},
+  {name: "Dr.SAM AHMED MAZUMDER sir", subject: "HUMANITIES"},
+  {name: "Dr.SUPREME DAS sir", subject: "METALLURGY"}
 ];
 
 var professor = "";
